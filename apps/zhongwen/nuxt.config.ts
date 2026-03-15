a510497c17298ca1f7c9e419c3e237c14af5efb9
Vadim <115623536+vadimvalov@users.noreply.github.com>
2026-03-15 20:22:48 +0500

    fix: formatting

--- apps/zhongwen/nuxt.config.ts
+++ apps/zhongwen/nuxt.config.ts
@@ -23,9 +23,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "中文",
-      meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-      ],
+      meta: [{ name: "viewport", content: "width=device-width, initial-scale=1.0" }],
       link: [{ rel: "icon", href: "/favicon.png" }],
     },
   },
